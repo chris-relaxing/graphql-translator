d8d08242ce7def9b74b89e15de2e88ee499eed00
Chris Nielsen <chris.nielsen@forumcomm.com>
2019-10-23 11:07:16 -0500

    First commit

---
+++ graphql-interpreter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+The purpose of this script is to take in a block of graphql and output the graphql's meaning
+in plain English.
+"""
+print('hola amigo')
+
+graphqlInput = """
+{
+  hero {
+    name
+  }
+}
+"""
